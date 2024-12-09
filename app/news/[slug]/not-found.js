b3aad010d284359ebe093376c2216131d79e1e31
Evan CHARRIER <evanc378549@gmail.com>
2024-12-09 20:42:47 +0100

    add a notfound page

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Article non trouvée</h1>
+      <p>L'article demandé ne semble pas exister.</p>
+    </div>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Page non trouvée</h1>
+      <p>La page demandée ne semble pas exister.</p>
+    </div>
+  );
+}
